9f209f80f1a264d8113f31f4c71c808332e14d79
pranav s <pranavofficial404@gmail.com>
2023-06-16 10:05:25 +0530

    some mod

--- customer/src/test/setup.ts
+++ customer/src/test/setup.ts
@@ -3,20 +3,20 @@ import mongoose from "mongoose";
 
 let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = "asdfasdf";
+  process.env.APP_SECRET = "asdfasdf";
   // process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
   // process.env.MONGO_URI = 'mongodb://localhost:27017/customer'
 
 
-  if (process.env.NODE_ENV ==='dev' ) {
+  // if (process.env.NODE_ENV ==='dev' ) {
     await mongoose.connect('mongodb://localhost:27017/customer')
-  } else {
-    const mongo = await MongoMemoryServer.create();
-    const mongoUri = mongo.getUri();
-    await mongoose.connect(mongoUri);
+  // } else {
+  //   const mongo = await MongoMemoryServer.create();
+  //   const mongoUri = mongo.getUri();
+  //   await mongoose.connect(mongoUri);
     
     
-  }
+  // }
 });
 
 // beforeEach(async () => {
