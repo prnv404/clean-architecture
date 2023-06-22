96d8c3a33979370e6fcee21629ee58b7d35d8bb3
pranav s <pranavofficial404@gmail.com>
2023-06-22 13:20:47 +0530

    proxy added

--- customer/src/server.ts
+++ customer/src/server.ts
@@ -2,12 +2,10 @@ import 'reflect-metadata'
 import 'express-async-errors'
 
 import 'reflect-metadata'
-import express from 'express';
 import { CUSTOMER_SERVICE, EXCHANGE_NAME, MONGO_URI, MSG_QUEUE_URL, PORT, configureIOCContainer } from './config';
 import { ConnectDb } from './framework/mongodb'
 import { CreateChannel, SubscribeMessage } from "@prnv404/ecom-common"
 import { ExpressApp } from './framework'; 
-import { CustomerUseCase } from './usecase/customer/customer.usecase';
 
 
 const StartServer = async () => {
